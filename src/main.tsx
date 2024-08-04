bcd12b932220ba49b0f49b651dca6371df9f79f3
Ilya Dragonromeo <you@example.com>
2024-08-04 14:25:56 +0300

    feat: add deploy settings

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/decent_test/'>
     <App />
   </BrowserRouter>
 );
